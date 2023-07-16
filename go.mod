2db889e6b76016ea21e8a4e9350f1ae511e08e95
Ishan Goel <igoel.mail@gmail.com>
2023-07-16 17:18:28 -0400

    greatly improve formatting and detail and make verbosity an option

--- go.mod
+++ go.mod
@@ -2,13 +2,18 @@ module wolframbot
 
 go 1.20
 
-require github.com/quackduck/devzat/devzatapi v0.0.0-20230715204053-9482a6a8b974
+require (
+	github.com/jwalton/gchalk v1.3.0
+	github.com/quackduck/devzat/devzatapi v0.0.0-20230715204053-9482a6a8b974
+)
 
 require (
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/jwalton/go-supportscolor v1.1.0 // indirect
 	github.com/quackduck/devzat/plugin v0.0.0-20230715201334-cc16f25360de // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/term v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 	google.golang.org/grpc v1.56.1 // indirect
--- main.go
+++ main.go
@@ -1,45 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
+	"github.com/jwalton/gchalk"
 	api "github.com/quackduck/devzat/devzatapi"
 )
 
 var (
-	devzatToken string
-	appID       string
+	devzatToken = os.Getenv("DEVZAT_TOKEN")
+	appID       = os.Getenv("WOLFRAM_APP_ID")
+	//wolfClient  = &wolfram.Client{AppID: appID}
+	name = gchalk.BrightMagenta("wolfram")
 )
 
 func main() {
-	devzatToken = os.Getenv("DEVZAT_TOKEN")
-	appID = os.Getenv("WOLFRAM_APP_ID")
-
 	s, err := api.NewSession("devzat.hackclub.com:5556", devzatToken)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	err = s.SendMessage(api.Message{Room: "#main", From: "wolfram", Data: "I am online."})
+	err = s.SendMessage(api.Message{Room: "#main", From: name, Data: "I am online."})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	err = s.RegisterCmd("wolf", "<query>", "Ask WolframAlpha a question", func(cmdCall api.CmdCall, err error) {
+	err = s.RegisterCmd("wolf", "[-v/--verbose] <query>", "Ask WolframAlpha a question", func(cmdCall api.CmdCall, err error) {
 		if err != nil {
 			fmt.Println(err)
-			s.SendMessage(api.Message{Room: cmdCall.Room, From: "wolfram", Data: "An error occurred." + err.Error()})
+			s.SendMessage(api.Message{Room: cmdCall.Room, From: name, Data: "An error occurred." + err.Error()})
 		}
-		answer, err := getAnswer(cmdCall.Args)
+		if len(cmdCall.Args) == 0 {
+			s.SendMessage(api.Message{Room: cmdCall.Room, From: name, Data: "You need to ask a question."})
+			return
+		}
+		f := strings.Fields(cmdCall.Args)
+		verbose := false
+		for i := 0; i < len(f); i++ {
+			if f[i] == "-v" || f[i] == "--verbose" {
+				verbose = true
+				f = append(f[:i], f[i+1:]...)
+			}
+		}
+		answer, err := getAnswer(strings.Join(f, " "), verbose)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		err = s.SendMessage(api.Message{Room: cmdCall.Room, From: "wolfram", Data: answer})
+		err = s.SendMessage(api.Message{Room: cmdCall.Room, From: name, Data: answer})
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -56,14 +69,57 @@ func main() {
 	}
 }
 
-func getAnswer(question string) (string, error) {
-	r, err := http.Get("http://api.wolframalpha.com/v1/result?output=json&i=" + url.QueryEscape(question) + "%3F&appid=" + appID)
+func getAnswer(question string, verbose bool) (string, error) {
+	response := ""
+	r, err := http.Get("http://api.wolframalpha.com/v2/query?output=json&input=" + url.QueryEscape(question) + "%3F&appid=" + appID)
 	if err != nil {
 		return "", err
 	}
-	response, err := io.ReadAll(r.Body)
+	res := &WolframAPIResult{}
+	err = json.NewDecoder(r.Body).Decode(res)
 	if err != nil {
 		return "", err
 	}
-	return string(response), nil
+	for _, pod := range res.Result.Pods {
+		if !verbose {
+			if pod.Title == "Result" {
+				response = gchalk.BrightYellow(pod.Subpods[0].Plaintext)
+				break
+			}
+			if pod.Title == "Input interpretation" || pod.Title == "Input" {
+				continue
+			}
+		}
+		if len(pod.Subpods) == 0 || pod.Subpods[0].Plaintext == "" {
+			continue
+		}
+		if pod.Title != "Result" {
+			response += pod.Title + ": "
+		}
+		for _, subpod := range pod.Subpods {
+			if subpod.Plaintext == "" {
+				continue
+			} else if pod.Title == "Result" {
+				response += gchalk.BrightYellow(subpod.Plaintext) + "  \n"
+			} else {
+				response += subpod.Plaintext + "  \n"
+			}
+		}
+		if pod.Title == "Result" {
+			response += "  \n"
+		}
+	}
+	return response, nil
+}
+
+type WolframAPIResult struct {
+	Result struct {
+		Pods []struct {
+			Title   string `json:"title"`
+			Subpods []struct {
+				Title     string `json:"title"`
+				Plaintext string `json:"plaintext"`
+			} `json:"subpods"`
+		} `json:"pods"`
+	} `json:"queryresult"`
 }
